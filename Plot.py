c5652fe8c64d4768615a34a74cdf249a69c179eb
qinyang <qinyang789@gmail.com>
2022-12-15 21:46:16 -0600

    PID Controller

--- Plot.py
+++ Plot.py
@@ -10,3 +10,18 @@ def noise():  # noise removal function
                 plt.subplot(122), plt.imshow(dst, 'gray'), plt.title("Noise_Removal")
                 plt.show()  # Show plot function
                 fig.savefig('out_5.jpg', dpi=200)  # Save the image into JPG format
+
+
+# 8. Visualize Predicted Price
+data = stock_data.filter(['Close'])
+train = data[:training_data_len]
+validation = data[training_data_len:]
+validation['Predictions'] = predictions
+plt.figure(figsize=(16,8))
+plt.title('Model')
+plt.xlabel('Date')
+plt.ylabel('Close Price USD ($)')
+plt.plot(train)
+plt.plot(validation[['Close', 'Predictions']])
+plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
+plt.show()
--- main.py
+++ main.py
@@ -1,51 +1,63 @@
+import matplotlib.pyplot as plt
+from numpy import diff
+import scipy.integrate as integrate
+
+
 class Input:
   def __init__(self, measured_process_variable):
     self.measured_process_variable = measured_process_variable
 
 
-class Controller:
-  def __init__(self, measured_process_variable, set_point, proportional_gain, integral_gain, derivative_gain):
+class PID_Controller:
+  def __init__(self, measured_process_variable, set_point, Kp, Ki, Kd):
     self.measured_process_variable = measured_process_variable
+    self.C = 1
     self.set_point = set_point
-    self.P_gain = proportional_gain
-    self. I_gain = integral_gain
-    self. D_gain = derivative_gain
-    self. difference = measured_process_variable - set_point
+    self.difference = measured_process_variable - set_point
+    self.P_gain = Kp
+    self.I_gain = Ki
+    self.D_gain = Kd
+    self.dt = 10
+    self.P = self.P_gain * self.difference
+    self.I = self. I_gain * self.difference / 2 * self.dt
+    self.D = self. D_gain * self.difference / self.dt
 
   def control(self):
-    controller_output = self.difference + 2 * self.D_gain
+    controller_output = self.P + self.I * + self.D
     return controller_output
 
 
-class Process:
+class Output_Process:
   def __init__(self, controller_output, disturbance):
     self.process_variable = controller_output + disturbance
 
 
-measured_process_variable = 0
-Input = Input(measured_process_variable)
+process_variable_pool = [0]
 
 
-measured_process_variable = 10
-set_point = 5
-proportional_gain = 30
-integral_gain = 40
-derivative_gain = 32
-Controller = Controller(measured_process_variable, set_point, proportional_gain, integral_gain, derivative_gain)
-
-controller_output = Controller.control()
-
-
-controller_output = 30
-disturbance = 0
-disturbance_array = [109, 23, 32, 43, 54]
-Process = Process(controller_output, disturbance)
-process_variable = Process.process_variable
+for n in range(10):
+    measured_process_variable = process_variable_pool[0]
+#   Input = Input(measured_process_variable)
+    set_point = 5
+    Kp = 30
+    Ki = 40
+    Kd = 32
+    Controller = PID_Controller(measured_process_variable, set_point, Kp, Ki, Kd)
+    controller_output = [0, Controller.control()]
+    disturbance_array = [109, 23, 32, 43, 54, 213, 12, 213, 2, 34]
+    Process = Output_Process(controller_output[-1], disturbance_array[n])
+    process_variable = Process.process_variable
+    process_variable_pool.append(process_variable)
 
 
-
-for n in disturbance_array:
-  disturbance = n
-  measured_process_variable = process_variable
-  print(disturbance, measured_process_variable, set_point, controller_output, process_variable)
+#   Visualize PID Control Process
+plt_1 = controller_output
+plt_2 = process_variable_pool
+plt.figure(figsize=(16, 8))
+plt.title('PID')
+plt.xlabel('Time')
+plt.ylabel('Measured Process Value')
+plt.plot(plt_1)
+plt.plot(plt_2)
+plt.show()
 
