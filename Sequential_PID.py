6c5dcfedf99ce3e524223c33dad92bd75c0e41d0
qinyang <qinyang789@gmail.com>
2022-12-23 17:47:09 -0500

    PID Controller

---
+++ Sequential_PID.py
@@ -0,0 +1,95 @@
+import matplotlib.pyplot as plt
+from numpy import diff
+import scipy.integrate as integrate
+import numpy as np
+
+time = np.arange(0, 10, 0.1)
+
+
+class Input:
+  def __init__(self, process_variable):
+    self.measured_process_variable = process_variable
+
+
+class PID_Controller:
+  def __init__(self, measured_process_variable, set_point):
+    self.measured_process_variable = measured_process_variable
+    self.C = 0
+    self.set_point = set_point
+    self.difference = setpoint - measured_process_variable
+    self.P_gain = 0.5
+    self.I_gain = 0
+    self.D_gain = 0
+    self.dt = 1
+    self.P = self.P_gain * self.difference
+    self.I = self. I_gain * self.difference * self.dt
+    self.D = self. D_gain * self.difference / self.dt
+
+
+  def control(self):
+    controller_output = self.C + self.P + self.I + self.D
+    round(controller_output, 2)
+    return controller_output
+
+
+class Output_Process:
+  def __init__(self, initial_state, controller_output, disturbance):
+    self.process_variable = initial_state + controller_output + disturbance
+
+
+process_variable_pool = [0]
+measured_variable_pool = [0]
+controller_output_pool = [0]
+disturbance = [0]
+plot_process_variable = [0]
+
+
+setpoint = 15
+controller_output = 0
+
+# disturbance = np.sin(time)
+disturbance = 0
+# disturbance = [12, 34, 32, 27, 30, 12, 123, 12, 43, 85]
+process_memory = 0
+n = 0
+
+while n < 10:
+  input = Input(process_variable_pool[0])
+  measured_variable_pool.insert(0, input.measured_process_variable)
+  if len(measured_variable_pool) > 4:
+      del measured_variable_pool[-1]
+
+
+  PID_controller = PID_Controller(measured_variable_pool[0], setpoint)
+  controller_output_pool.insert(0, PID_controller.control())
+  if len(controller_output_pool) > 4:
+    del controller_output_pool[-1]
+
+
+  output = Output_Process(process_memory, controller_output_pool[0], disturbance)
+  process_variable_pool.insert(0, output.process_variable)
+  process_memory = output.process_variable
+  if len(process_variable_pool) > 4:
+      del process_variable_pool[-1]
+  plot_process_variable.append(output.process_variable)
+  n = n + 1
+  print("n:", n, "Difference", PID_controller.difference)
+  print("n:", n, "Input", input.measured_process_variable, "Controller", PID_controller.control(), "Output", output.process_variable)
+
+
+print(plot_process_variable)
+
+#   Visualize PID Control Process
+# plt.plot(time, disturbance, setpoint, process_variable_pool[0])
+plt.axhline(y = setpoint, color = 'k', label = 'Setpoint')
+plt.axhline(y = disturbance, color = 'g', label = 'Disturbance')
+# plt.plot(test_disturbance, color = 'g', label = 'Disturbance')
+plt.plot(plot_process_variable, color = 'pink', label = 'Process')
+plt.title('PID Control Process')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.grid(True, which='both')
+# plt.axhline(y=0, color='k')
+plt.legend()
+plt.show()
+
--- main.py
+++ main.py
@@ -2,91 +2,122 @@ import matplotlib.pyplot as plt
 from numpy import diff
 import scipy.integrate as integrate
 import numpy as np
+import threading
+import time
+
 
 time = np.arange(0, 10, 0.1)
 
 
-class Input:
+# The input Object is reponsible for generating input into the system
+class Input(threading.Thread):
   def __init__(self, process_variable):
+    threading.Thread.__init__(self)
     self.measured_process_variable = process_variable
 
+  def run(self):
+    measured_variable_pool.insert(0, self.measured_process_variable)
+    if len(measured_variable_pool) > 5:
+      del measured_variable_pool[-1]
+    print("INPUT", measured_variable_pool)
+    return measured_variable_pool
 
-class PID_Controller:
+
+# Controller Object using input, feedback and setpoint to adjust controller signal
+class PID_Controller(threading.Thread):
   def __init__(self, measured_process_variable, set_point):
+    threading.Thread.__init__(self)
     self.measured_process_variable = measured_process_variable
-    self.C = 1
+    self.C = 0
     self.set_point = set_point
     self.difference = setpoint - measured_process_variable
-    self.P_gain = 0.5
-    self.I_gain = 0
-    self.D_gain = 0
+    self.P_gain = 0.8
+    self.I_gain = 0.05
+    self.D_gain = 0.02
     self.dt = 1
     self.P = self.P_gain * self.difference
     self.I = self. I_gain * self.difference * self.dt
     self.D = self. D_gain * self.difference / self.dt
 
 
-  def control(self):
-    controller_output = self.P + self.I + self.D
-    round(controller_output, 2)
-    return controller_output
+  def run(self):
+    controller_signal = self.C + self.P + self.I + self.D
+    round(controller_signal, 2)
+    controller_signal_pool.insert(0, controller_signal)
+#    controller_signal_pool.insert(0, 4)
+    print("PROCESS", controller_signal_pool)
+    if len(controller_signal_pool) > 4:
+      del controller_signal_pool[-1]
+    return controller_signal_pool
 
 
-class Output_Process:
-  def __init__(self, controller_output, disturbance):
-    self.process_variable = controller_output + disturbance
+# Output Object provide feedback
+class Output_Process(threading.Thread):
+  def __init__(self, initial_state, controller_output, disturbance):
+    threading.Thread.__init__(self)
+    self.initial_state = initial_state
+    self.controller_output = controller_output
+    self.disturbance = disturbance
 
+  def run(self):
+    self.process_variable = self.initial_state + self.controller_output + self.disturbance
+    process_variable_pool.insert(0, self.process_variable)
+    if len(process_variable_pool) > 5:
+      del process_variable_pool[-1]
 
+    print("OUTPUT", process_variable_pool, self.controller_output)
+    return process_variable_pool
+
+  def plot(self):
+    self.process_variable = self.initial_state + self.controller_output + self.disturbance
+    plot_process_variable.append(self.process_variable)
+    return plot_process_variable
+
+# Initial Parameters Setup
 process_variable_pool = [0]
 measured_variable_pool = [0]
-controller_output_pool = [0]
+controller_signal_pool = [0]
 disturbance = [0]
 plot_process_variable = [0]
 
-
 setpoint = 15
+controller_signal = 0
 
+
+# Try Different Disturbance Situations to verify PID Module
 # disturbance = np.sin(time)
-disturbance = 20
-# disturbance = [12, 34, 32, 27, 30]
+disturbance = 0
+# disturbance = [12, 34, 32, 27, 30, 12, 123, 12, 43, 85]
+
+
+# Create 3 threads
+thread_input = Input(process_variable_pool[0])
+thread_process = PID_Controller(measured_variable_pool[0], setpoint)
+thread_output = Output_Process(process_variable_pool[0], controller_signal_pool[0], disturbance)
+
+
+thread_input.start()
+thread_process.start()
+thread_output.start()
+
+
 n = 0
-
-
 while n < 10:
-  input = Input(process_variable_pool[0])
-  measured_variable_pool.insert(0, input.measured_process_variable)
-  if len(measured_variable_pool) > 4:
-      del measured_variable_pool[-1]
+  measured_variable_pool = Input(process_variable_pool[0]).run()
+  controller_signal_pool =PID_Controller(measured_variable_pool[0], setpoint).run()
+  process_variable_pool = Output_Process(process_variable_pool[0], controller_signal_pool[0], disturbance).run()
+  plot_process_variable = Output_Process(process_variable_pool[0], controller_signal_pool[0], disturbance).plot()
+  n = n + 0.5
 
-
-  PID_controller = PID_Controller(measured_variable_pool[0], setpoint)
-  controller_output_pool.insert(0, PID_controller.control())
-  if len(controller_output_pool) > 4:
-    del controller_output_pool[-1]
-
-
-  output = Output_Process(controller_output_pool[0], disturbance)
-  process_variable_pool.insert(0, output.process_variable)
-  if len(process_variable_pool) > 4:
-      del process_variable_pool[-1]
-  plot_process_variable.append(output.process_variable)
-  n = n + 1
-  print("n:", n, "Output.process_variable", output.process_variable)
-
-
-print(plot_process_variable)
 
 #   Visualize PID Control Process
-# plt.plot(time, disturbance, setpoint, process_variable_pool[0])
 plt.axhline(y = setpoint, color = 'k', label = 'Setpoint')
 plt.axhline(y = disturbance, color = 'g', label = 'Disturbance')
-# plt.plot(test_disturbance, color = 'g', label = 'Disturbance')
-plt.plot(plot_process_variable, color = 'k', label = 'Process')
+plt.plot(plot_process_variable, color = 'pink', label = 'Process')
 plt.title('PID Control Process')
 plt.xlabel('Time')
-plt.ylabel('Disturbance = sin(time)')
+plt.ylabel('Amplitude')
 plt.grid(True, which='both')
-# plt.axhline(y=0, color='k')
 plt.legend()
 plt.show()
 
